b845eb403074478981553fab73985cbbb83d30a2
Jaslan <Jaslan.dlnv@gmail.com>
2021-07-27 19:45:33 +0500

    add: Added Login and reducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import JoinBlock from './components/JoinBlock';
-import socket from './socket';
+import React from "react";
+import JoinBlock from "./components/JoinBlock";
+import reducer from "./reducer";
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, {
+    isAuth: false,
+  });
+
+  const onLogin = () => {
+    dispatch({
+      type: 'IS_AUTH',
+      payload: true
+    });
+  }
+
+  console.log(state);
 
   return (
-  <div className="wrapper">
-    <JoinBlock />
-  </div>
-  )
+    <div className='wrapper'>
+      {!state.isAuth && <JoinBlock onLogin={onLogin}/>}
+    </div>
+  );
 }
 
 export default App;
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import socket from '../socket'
+import axios from "axios";
+import React from "react";
+import socket from "../socket";
 
-const JoinBlock = () => {
+
+const JoinBlock = ({onLogin}) => {
+  const [roomId, setRoomId] = React.useState("");
+  const [userName, setUserName] = React.useState("");
+  const [isLoading, setLoading] = React.useState(false);
+
+
+  const onEnter = async () => {
+    if (!roomId || !userName) {
+      return alert("Invalid data");
+    }
+    setLoading(true)
+    await axios.post("/rooms", {
+      roomId,
+      userName,
+    });
+    onLogin();
+  };
   return (
-    <div className="join-block">
-      <input type="text"placeholder="Room ID"/>
-      <input type="text"placeholder="Your name"/>
-      <button className="btn btn-success">Enter</button>
+    <div className='join-block'>
+      <input
+        type='text'
+        placeholder='Room ID'
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type='text'
+        placeholder='Your name'
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button disabled={isLoading} onClick={onEnter} className='btn btn-success'>
+        {isLoading ? 'LOGIN...': 'ENTER'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default JoinBlock
+export default JoinBlock;
